737f5b8358ff7d32e802413e2e6674e83e726712
Alokendra <alokendra@gmail.com>
2020-04-04 11:11:35 +0530

    Added subtract function
    Added a function to subtract number.

--- test.py
+++ test.py
@@ -8,6 +8,13 @@ def add(x,y):
   """
   return x + y
 
+def subtract(x,y):
+  """
+  Subtracts two numbers
+  """
+  return x - y
+
 print os.getcwd()
 print "Hello World"
 print add(3 + 4)
+print subtract(x, y)
